faf2d8ee2b97d42a5541e619cb9ac512f6bfd1f9
abdul basit <abdulbasit2745@gmail.com>
2025-08-05 22:54:19 +0500

    change

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("what i want to become ");
+console.log("i want to become a Indutrial AI/Automation developer");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,2 @@
+console.log ("what i want to become ")
+console.log ("i want to become a Indutrial AI/Automation developer")
